005c18e367b6f3e098ef5791bc1f5230aab7e2a8
Wayne C. Gramlich <Wayne@Gramlich.Net>
2013-03-07 19:54:05 -0800

    Added High_GUI2* to package.

---
+++ High_GUI2_C.h
@@ -0,0 +1,19 @@
+/*
+ * Header for OpenCV.
+ *
+ * Copyright (c) 2010 by Wayne C. Gramlich
+ * All rights reserved.
+ */
+
+#ifndef HIGH_GUI2_C_H_INCLUDED
+#define HIGH_GUI2_C_H_INCLUDED 1
+
+#include "opencv2/highgui/highgui_c.h"
+
+typedef CvCapture *CV_Capture;
+typedef CvVideoWriter *CV_Video_Writer;
+
+extern CvCapture CV_Capture__Initial;
+extern CvVideoWriter CV_Video_Writer__Initial;
+
+#endif /* HIGH_GUI2_C_H_INCLUDED */
